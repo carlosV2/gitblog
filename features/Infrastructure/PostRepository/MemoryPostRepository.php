098e332491f6548c5a36fe1cf38b7e35fcd2ae4a
Carlos Ortega Huetos <carlos@rechannel.co.uk>
2015-08-16 00:47:13 +0100

    Create a MemoryPostRepository for testing

---
+++ features/Infrastructure/PostRepository/MemoryPostRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Infrastructure;
+
+use Everzet\PersistedObjects\CallbackObjectIdentifier;
+use Everzet\PersistedObjects\InMemoryRepository;
+use Everzet\PersistedObjects\Repository;
+use Gitblog\Post;
+use Gitblog\PostRepository;
+
+class MemoryPostRepository implements PostRepository
+{
+    /**
+     * @var Repository
+     */
+    private $repository;
+
+    public function __construct()
+    {
+        $this->repository = new InMemoryRepository(new CallbackObjectIdentifier(function (Post $post) {
+            return $this->getObjectIdentifierByPost($post);
+        }));
+    }
+
+    /**
+     * @param Post $post
+     *
+     * @return string
+     */
+    private function getObjectIdentifierByPost(Post $post)
+    {
+        return $this->getObjectIdentifierByAuthorAndName($post->getAuthor(), $post->getName());
+    }
+
+    /**
+     * @param string $author
+     * @param string $name
+     *
+     * @return string
+     */
+    private function getObjectIdentifierByAuthorAndName($author, $name)
+    {
+        return md5($author . '_' . $name);
+    }
+
+    /**
+     * @param string $author
+     * @param string $name
+     *
+     * @return Post
+     */
+    public function findByAuthorAndName($author, $name)
+    {
+        return $this->repository->findById($this->getObjectIdentifierByAuthorAndName($author, $name));
+    }
+}
